4273aa7153d05b8091d1b35ff6c43f4155dc3f4b
Sitick <90913758+Sitick@users.noreply.github.com>
2021-09-23 23:05:55 +0500

    Add files via upload

---
+++ lab1/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+
+ 
+int main()
+{
+  char a=5; //
+  char b=65;
+  char c=0;
+  c=a+b; 
+  std::cout << "c= " << c << std::endl;
+  char d=50;
+  char f=65;
+  char g=0;
+  g=d+f; 
+  std::cout << "g= " << g << std::endl;
+  char q=-10;
+  char w=1;
+  char e=0;
+  e=q+w; 
+  std::cout << "q= " << q << std::endl;
+  std::cout << "e= " << e << std::endl;
+  return 0;
+}
